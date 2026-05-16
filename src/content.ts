f5f61e9c071ac33a0dd60422b682f6c44b027686
Vicgital Admin <Vicgital@hotmail.com>
2026-05-16 14:30:35 -0600

    refine content

--- src/content.ts
+++ src/content.ts
@@ -41,7 +41,7 @@ export const pages: PageContent[] = [
       src: 'https://gabyletter.blob.core.windows.net/media/page-3.mp4',
       alt: 'Video mio programando la carta para gaby',
     },
-    text: "Cada instante a tu lado ha sido un gran regalo y hoy quiero regalarte esta carta en una web app que programé pensando en ti y en nosotros. Expresarte lo que siento por ti de una manera diferente y a mi estilo.\nEspero que disfrutes cada página tanto como yo disfruté creándola para ti...",
+    text: "Cada instante a tu lado ha sido un gran regalo y hoy quiero regalarte esta carta en una web app que programé pensando en ti y en nosotros. Expresar lo que siento por ti de una manera diferente y a mi estilo.\nEspero que disfrutes cada página tanto como yo disfruté creándola para ti...",
     effect: 'sparkles',
   },
   /// ... more pages here ...  
@@ -52,7 +52,7 @@ export const pages: PageContent[] = [
       src: 'https://gabyletter.blob.core.windows.net/media/page-4.mp4',
       alt: 'Video de nuestros días de baile juntos en la academia de baile',
     },
-    text: "Te conocí en mi refugio: la academia.\nY un día llegaste tú. Bailando contigo todo fluía desde el primer paso, y cada clase me daba más ganas de que llegara la siguiente. Tu sonrisa, tu belleza y tu olor me ponían nervioso. Y cada vez quería más conocerte fuera de cualquier clase o social.",
+    text: "Te conocí en lo que era un refugio para mi: la academia.\nY un día llegaste tú. Bailando contigo todo fluía desde el primer paso, y cada clase me daba más ganas de que llegara la siguiente. Tu sonrisa, tu belleza y tu olor me ponían nervioso (hasta la fecha). Cada vez quería más conocerte fuera del ambito del baile.",
     effect: 'petals',
   },
   {
@@ -62,7 +62,7 @@ export const pages: PageContent[] = [
       src: 'https://gabyletter.blob.core.windows.net/media/page-5.jpg',
       alt: 'Foto de nosotros en el Pa\'l Norte',
     },
-    text: 'Hasta que mi oportunidad llegó, estar contigo en Pa\'l Norte y entre la multitud, la música y contigo a mi lado, me sentí completamente yo. Quería que me conocieras a mi y a mi forma de disfrutar la vida.\nEsa noche entendí que mi conexión contigo no era solo bailando — era en todo lo demás también.',
+    text: 'Hasta que mi oportunidad llegó, estar contigo en Pa\'l Norte y entre la multitud, la música y contigo a mi lado, me sentí completamente yo. Quería que me conocieras a mi y a mi forma de disfrutar la vida.\nEsa noche entendí que mi conexión contigo no era solo bailando — era en mentalidad y corazón también.',
     effect: 'sparkles',
   },
   {
@@ -82,7 +82,7 @@ export const pages: PageContent[] = [
       src: 'https://gabyletter.blob.core.windows.net/media/page-7.mp4',
       alt: 'Video de nuestras dates',
     },
-    text: 'Desde ese día me dediqué a enamorarte. Y tú, quizás con miedo o sin saberlo, hiciste lo mismo conmigo. Te mostré mi lado más vulnerable, y tú me mostraste el tuyo. Entre más te conocía, más te admiraba, más me enamoraba de ti y de tu hermosa sonrisa.',
+    text: 'Desde ese día me dediqué a enamorarte. Y tú, quizás con miedo o sin saberlo, hiciste lo mismo conmigo. Te mostré mi lado más vulnerable, y tú me mostraste el tuyo. Entre más te conocía, más te admiraba, más me estaba enamorando de ti y de tu hermosa sonrisa.',
     effect: 'sparkles',
   },
   {
@@ -92,7 +92,7 @@ export const pages: PageContent[] = [
       src: 'https://gabyletter.blob.core.windows.net/media/page-8.jpg',
       alt: 'Foto de gaby sosteniendo su celular',
     },
-    text: 'Eres una mujer admirable con un enorme corazón, ambiciosa, con una claridad sobre lo que quieres en la vida que pocas personas tienen. Eres mucho más inteligente que yo. Y eso, en lugar de intimidarme, lo atesoro como algo que quiero cuidar para siempre.',
+    text: 'Eres una mujer admirable y excepcional, con un enorme corazón, ambiciosa, con una claridad sobre lo que quieres en la vida que pocas personas tienen. Eres mucho más inteligente que yo. Y eso, en lugar de intimidarme, lo atesoro como algo que quiero cuidar para siempre.',
     effect: 'petals',
   },
   {
@@ -111,7 +111,7 @@ export const pages: PageContent[] = [
       type: 'video',
       src: 'https://gabyletter.blob.core.windows.net/media/page-10.mp4',
       alt: 'Video de gaby en el columpio de un parque sonriendo y feliz',
-    },
+    },    
     text: 'Quiero hacer todo lo que esté en mí para hacerte feliz, escogerte siempre sin importar la circunstancia. Ser tu refugio y tu acompañante en cada triunfo y adversidad en nuestras vidas, quiero recorrer la vida contigo.',
     effect: 'petals',
   },
